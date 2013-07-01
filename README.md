f1ec4b1e886da4b37c3f7132432840d7a542664e
Mind Cookin <mindcookin@gmail.com>
2013-07-01 20:06:26 +0200

    Updated README and added comments to server side
    Added docs
    Removed unnecessary resources

--- README.md
+++ README.md
@@ -1,51 +1,60 @@
 Kujenga Pamoja
 ============
 
-About the game
---------------
 Kujenga Pamoja is a kind of tribute to "Jenga", the block playing game. In fact, "Jenga" means "built" in Suajili, and "Kujenga Pamoja" means "build together".
 
 This said, the idea was to create a multiplayer experience based on the use of socket communications, connecting mobile phones with a desktop application, using the phones like a gamepad while desktop application acts like the machine/console. 
 
 How to play
 --------------
-First of all, open kujenga-pamoja.appspot.com in Chrome( at this time, it is not avalaible in other browsers ). You will be presented with an URL. Introduce the URL in your smartphone (this time you can use any browser) and follow the instructions. You should be able to play with the "machine" (desktop) from your mobile phone.
+First of all, open the [game](http://2.kujenga-pamoja.appspot.com) in Chrome( at this time, it is not avalaible in other browsers ). You will be presented with an URL. Introduce the URL in your smartphone (this time you can use any browser) and follow the instructions. You should be able to play with the "machine" (desktop) from your mobile phone.
 
 Note1 : Enable Wifi connection in your smartphone. If you don't have a good bandwith, you will experiment strange behaviours.
 Note2 : If you don't have any smartphone, just open the given URL in a new tab. It is not so beatiful but is much more handy.
 
-Technical issues
+How it works
 --------------
-The language I chose was javascript, the native language to use with HTML5's canvas 3D context. That means that Kujenga Pamoja is a web application and will only run within a browser. In fact, actually it's only running in Chrome, because THREE.js WebGLRenderer is not supported in other browser.
+Basically the machine (the application on your desktop browser) opens a communication channel with the player( mobile phone) that let the server receive and send messages inbetween them in nearly real time. Now we can use the phone as a gamepad and update the game in real time.
 
-THREE.js abstracts WebGL plain code, and is much easier to develop than plain WebGL. Udacity's [Interactive 3D Graphics course](https://www.udacity.com/course/cs291) uses this library, and basically I learn nearly all I know of THREE from this course. [Physijs](http://chandlerprall.github.io/Physijs/) was my physics engine pick. Well it is not really a physics engine, it is a layout over [Ammo.js](https://github.com/kripken/ammo.js/) that implements methods for THREE.js development.
+In the code side, we need to create a channel from the Google App Engine javascript Channel API. This is made at ConnectionClass ( there is one Connection for each side: one for machine and one for player ). Once we have the connection ready, we can send messages between machine and player. We have to take care that each message pass through our server, and he handles were to distribute it. 
 
-For interactive mobile communications I chose [Google App Engine Channel](https://developers.google.com/appengine/docs/python/channel/). It creates a socket and stablish open communications from client to server and reverse. Easy to start with and easy to use. To use it I need my project to be hosted on [Google App Engine](https://developers.google.com/appengine/). I chose Python for my server language (I didn't use it before) and it was a really good surprise. As a front-end developer, I enjoyed very much the sintaxis.
+In the very first step ( when we first open the "machine" and ask the server for a Channel ), the server creates a unique identifier for our Channel (the token) and gives the machine a unique identifier to identify that user. We create a url with some get parameters that let us identify players when they connect to our server. When a new player is connected, the server just give him a new identifier, without modifying the token. 
 
-I didn't work with HTML5's 3D context before, and neather with sockets, so I have to thank these tools developers to make them so easy to use.
+We store the actual state of the game in the server ( initialized, ready, ongame, lose, etc... ). This is send at the same time to all the ConnectionClass instances, and each player and machine updates its state accordingly. In the other side, both machine and player send messages to the server whenever a button is pressed or when the player loses or if the game is ready to start...
+
+More 
+--------------
+For further information on how the game was made please read the [documentation](http://2.kujenga-pamoja.appspot.com/docs/docs.html).
 
 Resources
 ---------
 ### Libraries
 The incredible [THREE.js](http://threejs.org/) for WebGL and 3D.
-[Physijs](http://chandlerprall.github.io/Physijs/) for physics integration in THREE.
-[Google App Engine Channel](https://developers.google.com/appengine/docs/python/channel/) for socket communications.
-[TweenMax](http://www.greensock.com/tag/javascript/)
-[jQuery](http://jquery.com/)
-[Closure compiler](https://developers.google.com/closure/compiler/) to compress the code. Really useful.
-Hosted on [Google App Engine](https://developers.google.com/appengine/)
 
-### Game Design and Source Code and Graphics
-Kujenga Pamoja by Santiago Bernabé García ( [Mind Cookin](http://mindcookin.com) )[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
+[Physijs](http://chandlerprall.github.io/Physijs/) for physics integration in THREE.
+
+[Google App Engine Channel](https://developers.google.com/appengine/docs/python/channel/) for socket communications.
+
+[TweenMax](http://www.greensock.com/tag/javascript/)
+
+[jQuery](http://jquery.com/)
+
+[Closure compiler](https://developers.google.com/closure/compiler/) to compress the code. Really useful.
+
+Hosted on [Google App Engine](https://developers.google.com/appengine/)
 
 ### Fonts :
 [Commando](http://www.fontsquirrel.com/fonts/Commando)[CC BY-SA 3.0](http://creativecommons.org/licenses/by-nc-sa/3.0/).
 
 ### Music and sounds :
 [Lift](http://www.flashkit.com/soundfx/Interfaces/Blips/Lift_Me_-Vyachesl-8596/index.php)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
-[Click](http://www.flashkit.com/soundfx/Interfaces/Clink-Intermed-476/index.php)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
-[Fairyland](http://www.freesound.org/people/AlienXXX/sounds/82328/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
-[Life Lost](http://www.freesound.org/people/noirenex/sounds/159408/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
-[8bit Coin](http://www.freesound.org/people/timgormly/sounds/170147/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
-[Coinup](http://www.freesound.org/people/mattwasser/sounds/58919/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
 
+[Click](http://www.flashkit.com/soundfx/Interfaces/Clink-Intermed-476/index.php)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+[Fairyland](http://www.freesound.org/people/AlienXXX/sounds/82328/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+[Life Lost](http://www.freesound.org/people/noirenex/sounds/159408/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+[8bit Coin](http://www.freesound.org/people/timgormly/sounds/170147/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+[Coinup](http://www.freesound.org/people/mattwasser/sounds/58919/)[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
--- kujengapamoja.py
+++ kujengapamoja.py
@@ -14,9 +14,11 @@ import random
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
-
+	
+"""
+All the data we store for a game
+"""
 class Game(db.Model):
-	"""All the data we store for a game"""
 	machine = db.StringProperty()
 	user1 	= db.StringProperty()
 	user2 	= db.StringProperty()
@@ -28,12 +30,16 @@ class Game(db.Model):
 	loose  	= db.BooleanProperty()
 	state  	= db.IntegerProperty()
 
+"""
+Handles game updates and creates Game model
+"""
 class GameUpdater():
 	game = None
 
 	def __init__(self, game):
 		self.game = game
 
+	"""Returns a json containing the information in Game model"""
 	def get_game_message(self):
 		gameUpdate = {
 			"machine" 	: self.game.machine,
@@ -49,23 +55,33 @@ class GameUpdater():
 		}
 		return json.dumps(gameUpdate)
 
+	"""Sends update notifications to the players and machine"""
 	def send_update(self):
+
+		#retrieves the actual game model information
 		message = self.get_game_message()
+		
+		#send message to machine
 		channel.send_message( self.game.machine + self.game.key().id_or_name(), message)
 
-		if self.game.user1:
+		if self.game.user1:	#send message to user1, if exists
 			channel.send_message( self.game.user1 + self.game.key().id_or_name(), message)
-		if self.game.user2:
+		if self.game.user2:	#send message to user2, if exists
 			channel.send_message( self.game.user2 + self.game.key().id_or_name(), message)
-		if self.game.user3:
+		if self.game.user3:	#send message to user3, if exists
 			channel.send_message( self.game.user3 + self.game.key().id_or_name(), message)
-		if self.game.user4:
+		if self.game.user4:	#send message to user4, if exists
 			channel.send_message( self.game.user4 + self.game.key().id_or_name(), message)
 	
+	"""Sends close notifications to the machine"""
 	def send_close(self, user_id):
 		message = json.dumps( { "closed" : user_id } )
 		channel.send_message( self.game.machine + self.game.key().id_or_name(), message)
-		  
+
+"""
+Returns the actual game corresponding to the given game_key
+Essential to track the correct game model.
+"""		
 class GameFromRequest():
 	game = None;
 
@@ -78,49 +94,84 @@ class GameFromRequest():
 	def get_game(self):
 		return self.game
 
+"""
+Sent by Player whenever a key is pressed.
+Notifies machine and players that a key has been pressed.
+"""		
 class Pressed(webapp2.RequestHandler):
 
 	def post(self):
 		game = GameFromRequest(self.request).get_game()
 		user = self.request.get('u')
 
+		#Check the current key pressed and sends update if we are in gameplay
 		if game and user and user == game.active and game.state >= 3 and game.state < 6:
+			
+			#retrieve the key pressed
 			keyPressed = int( self.request.get('press') )
+			
+			#if keyPressed is lower than 3 means that an arrow key has been pressed
+			#updates the game model's state, set press to the corresponding arrow key
 			if keyPressed < 4 :
 				game.press 		= keyPressed
 				game.accuracy 	= bool( self.request.get('accuracy') == "true" )
 				game.put()
+				
+				#send update
 				GameUpdater(game).send_update()
 			else :
+				#if keyPressed is higher than 3 means that OK key has been pressed
+				#updates the game model's state, set press to -1 to indicate that no arrow key has been pressed
 				game.press = -1
 				game.state += 1
 				game.put()
+				
+				#send update
 				GameUpdater(game).send_update()
 
+"""
+Sent by Machine after a good move.
+Updates state and send updates to all connections (players and machine).
+"""		
 class MoveOK(webapp2.RequestHandler):
 
 	def post(self):
 		game = GameFromRequest(self.request).get_game()
 		user = self.request.get('u')
 
+		#updates the game model's state, set press to -1 to indicate that no arrow key has been pressed
 		if game and user:
 			game.state += 1
 			game.press = -1
 			game.put()
+			
+			#send update
 			GameUpdater(game).send_update()
-	
+
+"""
+Sent by Machine or Player when a socket is opened.
+Notify all about the update;
+"""			
 class Opened(webapp2.RequestHandler):
 
 	def post(self):
 		game = GameFromRequest(self.request).get_game()
+			
+		# override key pressed, little trick for connecting when already in a started game
+		game.press = -1
 		GameUpdater(game).send_update()
 
+"""
+Sent by Player when a socket is closed.
+Notify only machine;
+"""			
 class Closed(webapp2.RequestHandler):
 
 	def post(self):
 		game = GameFromRequest(self.request).get_game()
 		user = self.request.get('u')
 		
+		#remove the user from game model
 		if game and user:
 			if game.user1 	== user :game.user1 = ""
 			elif game.user2 == user :game.user2 = ""
@@ -128,17 +179,26 @@ class Closed(webapp2.RequestHandler):
 			elif game.user4 == user :game.user4 = ""
 			game.put();
 		
+			#and send update
 			GameUpdater(game).send_close( user )
-		
+
+"""
+Sent by Machine
+Notify all that the game has started and set the active user/player
+"""					
 class StartGame(webapp2.RequestHandler):
 
 	def post(self):
 		
 		game = GameFromRequest(self.request).get_game()
+		
+		# set game state to PLAY_STARTGAME
 		game.state = 2
 		
+		#add user in a list to make easy to handle
 		usersList = [ game.user1, game.user2, game.user3, game.user4 ]
 		
+		#set active user in game model
 		if not game.active:
 			game.active = usersList[0]
 		else :
@@ -150,49 +210,64 @@ class StartGame(webapp2.RequestHandler):
 				game.active = usersList[0]
 			
 		game.put()
-		GameUpdater(game).send_update()
 		
+		# send update
+		GameUpdater(game).send_update()
+
+"""
+Sent by Player after receiving StartGame
+Notify the machine and players that we can start selecting blocks
+"""							
 class Activated(webapp2.RequestHandler):
 
 	def post(self):
 		game = GameFromRequest(self.request).get_game()
+		
+		# set game state to PLAY_SELECT
 		game.state = 3;
 		game.put()
 		GameUpdater(game).send_update()
 
+"""
+Sent by Machine whenever a player loses
+Notify all that a player loses
+"""							
 class Lose(webapp2.RequestHandler):
 
 	def post(self):
 		game = GameFromRequest(self.request).get_game()
+		# set game state to LOSE
 		game.state = 8;
 		game.put()
 		GameUpdater(game).send_update()
-		
+
+"""
+Called when opening the main url or the player url
+Initializes game model, creates new token and user/machine identifiers, updates all changes and send notifications to all
+"""									
 class Main(webapp2.RequestHandler):
 
     def get(self):
 		
-		#user = users.get_current_user()
+		#Retrieve the u GET parameter from url. 
+		#If exists means that this user already exists
 		user = self.request.get('u')
 		
+		#If there is no user we create a new one and give him a new identifier
 		if not user:
 			user = id_generator(21, string.digits )
-			#self.redirect(users.create_login_url(self.request.uri))	
-			#return
-			
-		#Si hay game_key, significa que es un jugador
-		#Si no hay game_key, es la maquina
 		
+		#Retrieve g GET parameter, the game_key
 		game_key = self.request.get('g') 
-		#user_id = user.user_id()
 		
-		logging.info('USER:' + self.request.get('u') )
-		
+		# if there is no game_key means that this is the machine 
+		# and we need to create a new one
 		if not game_key :
 			
+			# create a new game_key that will be distributed to users by the URL
 			game_key = id_generator()
 			
-			# y creo un nuevo Game
+			# create a new game model
 			game = Game(
 				key_name = game_key,
 				machine = user,
@@ -201,12 +276,12 @@ class Main(webapp2.RequestHandler):
 			game.put()
 		else :
 		
-			# recojo el Game
+			# if there is a game_key, means that we have a Game, and we retrieve it
 			game = Game.get_by_key_name(game_key)
 			
-			#compruebo si existe usuario
+			#check is the user exists
 			if user != game.user1 and user != game.user2 and user != game.user3 and user != game.user4 :
-				# creo el usuario si no existe
+				# if it don't, we create it and add him to game model
 				if not game.user1 :
 					game.user1 = user
 				elif not game.user2 :
@@ -216,23 +291,25 @@ class Main(webapp2.RequestHandler):
 				elif not game.user4 :
 					game.user4 = user
 				
+				# and update game state in consecuence
 				if game.state == 0:
 					game.state = 1
-					
+				
 				game.put()
 			else :
+				# if already exists, show a message that this user has connected before ( shouldn't happen anyway )
 				template_values = { 'user_disabled' : "true" }
 				template = JINJA_ENVIRONMENT.get_template('player.html')
 				self.response.write(template.render(template_values))
 				return
 				
-		#recojo el token
+		#retrieve game token
 		token = channel.create_channel( user + game_key )
 		
-		#creo la URL
+		#and create the url ( we will only use it in machine)
 		game_url = self.request.url + "?g=" + game_key
 		
-		# Actualizo el HTML:
+		#update template values
 		template_values = {
 			'token': token,
 			'game_key': game_key,
@@ -245,6 +322,8 @@ class Main(webapp2.RequestHandler):
 			'me': user
 		}
 		
+		#and create the HTML corresponding to the Player or Machine, 
+		#depending if there is g GET parameter or not
 		if self.request.get('g'):
 			template = JINJA_ENVIRONMENT.get_template('player.html')
 			self.response.write(template.render(template_values))
@@ -252,9 +331,16 @@ class Main(webapp2.RequestHandler):
 			template = JINJA_ENVIRONMENT.get_template('machine.html')
 			self.response.write(template.render(template_values))
 			
+"""
+a simple id generator 
+used when creating a game_key and user id
+"""
 def id_generator(size = 6 , chars = string.ascii_uppercase + string.digits ):
 	return ''.join(random.choice(chars) for x in range(size))
-			
+
+"""
+Application rooting
+"""			
 application = webapp2.WSGIApplication([
     ('/', Main),
     ('/opened', Opened),
